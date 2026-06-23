fb3619b7f1a2dc200510289de3b8e333c191635a
Anchal Tiwari <tiwarianchal539@gmail.com>
2026-06-23 15:49:08 +0530

    Day 8 Practice

---
+++ breakstatement.py
@@ -0,0 +1,15 @@
+#for loop using break
+for i in range(1,6):
+    if i==3:
+        break
+    print(i)
+
+#while loop using break
+i=1
+while i<=5:
+    if i==4:
+     break
+    print(i)
+    i+=1
+
+    
---
+++ passStatement.py
@@ -0,0 +1,17 @@
+#with for loop
+for i in range(5):
+    pass
+print("loop finished")
+
+
+#with if statement
+x=10
+if x>5:
+    pass
+print("hello")
+
+
+#with function
+def my_function():
+    pass
+print("function created")
